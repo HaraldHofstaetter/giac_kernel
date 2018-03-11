fb7d4aa7afb9334e8b5be0345102bfadcaef4656
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2018-03-11 20:16:05 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='giac_kernel',
+      version='0.0.1',
+      description='Giac kernel for Jupyter',
+      author='Harald Hofstätter',
+      author_email='hofi@harald-hofstaetter.at',
+      url='https://github.com/HaraldHofstaetter/giac_kernel',
+      packages=['giac_kernel'],
+      scripts=['giac_kernel/install'],
+      keywords=['jupyter', 'notebook', 'kernel', 'giac'],
+      include_package_data=True
+      )
